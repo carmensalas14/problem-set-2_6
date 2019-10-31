dcba623422014527139e77739fb2a2d8e96bf324
Reuben Ogbonna <reubenogbonna@gmail.com>
2019-10-31 17:38:01 -0400

    added project exercises

---
+++ exercises/exercises.js
@@ -0,0 +1,25 @@
+// Question 1
+const log = () => {
+
+}
+
+// Question 2
+const totalProps = () => {
+
+}
+
+// Question 3
+const listBooks = () => {
+
+}
+
+// Question 4
+const repeatedCharacters = () => {
+
+}
+
+// Don't write below this line
+module.exports = {
+  totalProps,
+  repeatedCharacters,
+}
